8eeeeb14a38f8e2bb6723403ffec40c9b0a3238b
Amina Assaid <118275761+aminaassaid1@users.noreply.github.com>
2023-01-09 17:01:06 +0100

    Add files via upload

--- cuisine.js
+++ cuisine.js
@@ -24,4 +24,37 @@ for(let i = 0 ; i <6 ;i++){
 })
 }
 
-document.addEventListener("")
+document.querySelector('#search-btn').addEventListener('click',()=>{
+        document.querySelector('#cards').innerHTML = ''
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${document.querySelector('#search').value}`).then((response)=>{
+                let searchdata = response.json();
+                return searchdata
+                
+        }).then((response)=>{
+                response.meals.forEach(element => {
+                        let card = document.createElement("div");
+                        card.setAttribute("class","card col-md-5 col-lg-3 p-0 m-0")
+                        let img = document.createElement("img");
+                        img.setAttribute("src",element.strMealThumb);
+                        img.setAttribute("class","card-img-top")
+                        card.append(img);
+                        let CardBody = document.createElement("div");
+                        CardBody.setAttribute("class","card-body");
+                        let CardTitle = document.createElement("h5");
+                        CardTitle.setAttribute("class", "card-title");
+                        CardTitle.append(element.strMeal);
+                        let button = document.createElement("button");
+                        button.setAttribute("class","btn btn-primary");
+                        let show = document.createTextNode("Read More");
+                        button.appendChild(show);
+                        CardBody.append(CardTitle,button);
+                        card.append(CardBody);
+                        document.querySelector('#cards').append(card)
+                });
+        })
+
+})
+        //  pagination 
+
+var pagination = document.createElement("ul");
+pagination.setAttribute("class","pagination");
